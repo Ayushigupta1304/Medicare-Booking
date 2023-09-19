f66abfae12582432c207898ddbdf1b960407047f
AYUSHI GUPTA <125629327+Ayushigupta1304@users.noreply.github.com>
2023-09-19 22:10:32 +0530

    Fixed router issue

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import heroImg01 from "../assets/image/hero-img01.png";
 import heroImg02 from "../assets/image/hero-img02.png";
 import heroImg03 from "../assets/image/hero-img03.png";
